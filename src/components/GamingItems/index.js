1e55ab989368007ceb0175bea6fad14d505f1ecc
kumar <kumardevarapalli5@gmail.com>
2024-02-12 13:59:50 +0000

    main

--- src/components/GamingItems/index.js
+++ src/components/GamingItems/index.js
@@ -1,15 +1,18 @@
+import {Link} from 'react-router-dom'
 import './index.css'
 
 const GamingItems = props => {
   const {gaming} = props
-  const {viewCount, thumbnailUrl, title} = gaming
+  const {id, viewCount, thumbnailUrl, title} = gaming
   return (
     <>
-      <li className="gaming-list-container">
-        <img src={thumbnailUrl} alt="thumbnail" className="gaming-images" />
-        <h1 className="gaming-title">{title}</h1>
-        <p className="gaming-views">{viewCount} Watching worldwide</p>
-      </li>
+      <Link to={`/videos/${id}`} className="link">
+        <li className="gaming-list-container">
+          <img src={thumbnailUrl} alt="thumbnail" className="gaming-images" />
+          <h1 className="gaming-title">{title}</h1>
+          <p className="gaming-views">{viewCount} Watching worldwide</p>
+        </li>
+      </Link>
     </>
   )
 }
--- src/components/VideoItemsDetailsRoute/index.js
+++ src/components/VideoItemsDetailsRoute/index.js
@@ -1,11 +1,14 @@
 import {Component} from 'react'
 import Cookies from 'js-cookie'
+import {AiOutlineLike, AiOutlineDislike} from 'react-icons/ai'
+import {HiSaveAs} from 'react-icons/hi'
 import NavBarTop from '../NavBarTop'
 import NavBarSide from '../NavBarSide'
+import SavedVideosRoute from '../SavedVideosRoute'
 import './index.css'
 
 class VideoItemsDetailsRoute extends Component {
-  state = {videosUpdatedData: []}
+  state = {videosUpdatedData: [], like: false, disLike: false, save: false}
 
   componentDidMount() {
     this.getVideosData()
@@ -47,26 +50,104 @@ class VideoItemsDetailsRoute extends Component {
     }
   }
 
+  onClickLike = () => {
+    this.setState(prevState => ({like: !prevState.like}))
+  }
+
+  onClickDisLike = () => {
+    this.setState(prevState => ({disLike: !prevState.disLike}))
+  }
+
+  onClickSave = () => {
+    this.setState(prevState => ({save: !prevState.save}))
+  }
+
   render() {
-    const {videosUpdatedData} = this.state
+    const {videosUpdatedData, like, disLike, save} = this.state
     const videoId = String(videosUpdatedData.videoUrl)
     const urlId = videoId.split('=')[1]
     console.log(videosUpdatedData)
+
+    const likingIcon = like ? 'like-icon icon' : 'like-icon'
+    const likingText = like ? 'like-text text' : 'like-text'
+
+    const disLikingIcon = disLike ? 'like-icon icon' : 'like-icon'
+    const disLikingText = disLike ? 'like-text text' : 'like-text'
+
+    const saveIcon = save ? 'like-icon icon' : 'like-icon'
+    const saveText = save ? 'like-text text' : 'like-text'
+    const savedText = save ? 'Saved' : 'Save'
+
     return (
       <div>
         <NavBarTop />
-        <div>
+        <div className="nav-para">
           <NavBarSide />
-          <div>
+          <div className="video-container">
             <iframe
-              width="560"
-              height="315"
+              width="1150"
+              height="500"
               src={`https://www.youtube.com/embed/${urlId}`}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             />
+            <p className="title">{videosUpdatedData.title}</p>
+            <div className="views-likes-container">
+              <div className="views-published-container">
+                <p className="views">
+                  {videosUpdatedData.viewCount}
+                  <span className="views-span"> views</span>
+                </p>
+                <p className="dot">.</p>
+                <p className="published">{videosUpdatedData.publishedAt}</p>
+              </div>
+              <div className="like-dislike-container">
+                <button
+                  type="button"
+                  className="like-like-text-container"
+                  onClick={this.onClickLike}
+                >
+                  <AiOutlineLike className={likingIcon} />
+                  <p className={likingText}>Like</p>
+                </button>
+                <button
+                  type="button"
+                  className="like-like-text-container"
+                  onClick={this.onClickDisLike}
+                >
+                  <AiOutlineDislike className={disLikingIcon} />
+                  <p className={disLikingText}>Dislike</p>
+                </button>
+                <button
+                  type="button"
+                  className="like-like-text-container"
+                  onClick={this.onClickSave}
+                >
+                  <HiSaveAs className={saveIcon} />
+                  <p className={saveText}>{savedText}</p>
+                </button>
+              </div>
+            </div>
+            <hr className="horizontal-line" />
+            <div className="profile-container">
+              <img
+                src={videosUpdatedData.profileImageUrl}
+                alt="profile"
+                className="profile-image"
+              />
+              <div className="name-description-container">
+                <div className="name-subscriber-count">
+                  <p className="name">{videosUpdatedData.name}</p>
+                  <p className="subscriber-count">
+                    {videosUpdatedData.subscriberCount}
+                    <span className="subscriber-span"> subscribers</span>
+                  </p>
+                </div>
+                <p className="description">{videosUpdatedData.description}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
